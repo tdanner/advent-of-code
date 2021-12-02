f233a2153cd291599cea7e85b0ee3b390912491b
Tim Danner <tim@danners.us>
2021-12-02 14:42:51 -0600

    Day 2 first half

---
+++ day2/Program.cs
@@ -0,0 +1,23 @@
+﻿string[] lines = File.ReadAllLines("input.txt");
+int position = 0;
+int depth = 0;
+foreach (string line in lines) {
+    string[] parts = line.Split(' ');
+    string direction = parts[0];
+    int amount = int.Parse(parts[1]);
+    switch (direction) {
+        case "forward":
+            position += amount;
+            break;
+        case "down":
+            depth += amount;
+            break;
+        case "up":
+            depth -= amount;
+            break;
+        default:
+            throw new ArgumentOutOfRangeException("direction", direction);
+    }
+}
+
+Console.WriteLine($"position: {position}, depth: {depth}, product: {position * depth}");
