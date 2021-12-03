5a027de17eef56e07dcada0a513bdff052839768
Tim Danner <tim@danners.us>
2021-12-03 08:41:16 -0600

    Day 3 first half

---
+++ day3/Program.cs
@@ -0,0 +1,23 @@
+﻿string[] lines = File.ReadAllLines("input.txt");
+int bitCount = lines[0].Length;
+int[] ones = new int[bitCount];
+foreach (string line in lines) {
+    for (int i = 0; i < bitCount; ++i) {
+        if (line[i] == '1') {
+            ones[i]++;
+        }
+    }
+}
+
+uint gamma = 0;
+for (int i = 0; i < bitCount; ++i) {
+    gamma <<= 1;
+    if (ones[i] > lines.Length/2) {
+        gamma++;
+    }
+}
+
+uint mask = (((uint)1)<<bitCount) - 1;
+uint epsilon = ~gamma & mask;
+
+Console.WriteLine($"gamma: {gamma}, epsilon: {epsilon}, power: {gamma*epsilon}");
