6e97d3b599496bd7cb0e8f3319af4e417369d95e
Tim Danner <tim@danners.us>
2021-12-03 10:42:45 -0600

    Day 3 second half

--- day3/Program.cs
+++ day3/Program.cs
@@ -1,23 +1,70 @@
 ﻿string[] lines = File.ReadAllLines("input.txt");
 int bitCount = lines[0].Length;
 int[] ones = new int[bitCount];
-foreach (string line in lines) {
-    for (int i = 0; i < bitCount; ++i) {
-        if (line[i] == '1') {
+foreach (string line in lines)
+{
+    for (int i = 0; i < bitCount; ++i)
+    {
+        if (line[i] == '1')
+        {
             ones[i]++;
         }
     }
 }
 
 uint gamma = 0;
-for (int i = 0; i < bitCount; ++i) {
+for (int i = 0; i < bitCount; ++i)
+{
     gamma <<= 1;
-    if (ones[i] > lines.Length/2) {
+    if (ones[i] > lines.Length / 2)
+    {
         gamma++;
     }
 }
 
-uint mask = (((uint)1)<<bitCount) - 1;
+uint mask = (((uint)1) << bitCount) - 1;
 uint epsilon = ~gamma & mask;
+uint powerConsumption = gamma * epsilon;
+Console.WriteLine(new { gamma, epsilon, powerConsumption });
 
-Console.WriteLine($"gamma: {gamma}, epsilon: {epsilon}, power: {gamma*epsilon}");
+//////////////////////////////////////////////////////////////
+
+List<string> nums = lines.ToList();
+
+int CountOnesInPosition(int pos)
+{
+    int ones = 0;
+    foreach (string num in nums)
+    {
+        if (num[pos] == '1')
+        {
+            ones++;
+        }
+    }
+
+    return ones;
+}
+
+for (int i = 0; i < bitCount; ++i)
+{
+    int ones2 = CountOnesInPosition(i);
+    char mostCommon = ones2 >= nums.Count / 2.0 ? '1' : '0';
+    nums = nums.Where(n => n[i] == mostCommon).ToList();
+}
+
+int oxygenGeneratorRating = Convert.ToInt32(nums.Single(), 2);
+
+nums = lines.ToList();
+
+for (int i = 0; i < bitCount && nums.Count > 1; ++i)
+{
+    int ones2 = CountOnesInPosition(i);
+    char leastCommon = ones2 >= nums.Count / 2.0 ? '0' : '1';
+    nums = nums.Where(n => n[i] == leastCommon).ToList();
+}
+
+int co2ScrubberRating = Convert.ToInt32(nums.Single(), 2);
+
+int lifeSupportRating = oxygenGeneratorRating * co2ScrubberRating;
+
+Console.WriteLine(new { oxygenGeneratorRating, co2ScrubberRating, lifeSupportRating });
