9d16d38447c0565b5a7c17b827600d3c1e1c388e
Tim Danner <tim@danners.us>
2021-12-06 09:09:27 -0600

    Day 6 first half

---
+++ day6/Program.cs
@@ -0,0 +1,22 @@
+﻿List<int> lanternfishAges = File.ReadAllText("input.txt").Split(',')
+    .Select(int.Parse).ToList();
+
+for (int day = 0; day < 80; day++)
+{
+    List<int> newFish = new();
+    foreach (int age in lanternfishAges)
+    {
+        if (age == 0)
+        {
+            newFish.Add(6);
+            newFish.Add(8);
+        }
+        else
+        {
+            newFish.Add(age - 1);
+        }
+    }
+    lanternfishAges = newFish;
+}
+
+Console.WriteLine(new { lanternfishCount = lanternfishAges.Count });
