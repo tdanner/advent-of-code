a70a8dcd3886b44aa117fdef4d4cb69f84ef6096
Tim Danner <tim@danners.us>
2021-12-07 11:13:07 -0600

    Day 7 first half

---
+++ day7/Program.cs
@@ -0,0 +1,5 @@
+﻿int[] crabs = File.ReadAllText("input.txt").Split(',').Select(int.Parse).ToArray();
+Array.Sort(crabs);
+int median = crabs[crabs.Length / 2];
+int fuel = crabs.Select(crab => Math.Abs(crab - median)).Sum();
+Console.WriteLine(new { median, fuel });
