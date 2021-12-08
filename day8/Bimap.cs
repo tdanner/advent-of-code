0444670e297907a07761496113c80b1b2953d2aa
Tim Danner <tim@danners.us>
2021-12-08 10:39:50 -0600

    Day 8 second half

---
+++ day8/Bimap.cs
@@ -0,0 +1,101 @@
+using System.Collections;
+using System.Diagnostics.CodeAnalysis;
+
+public class Bimap<T1, T2> : IDictionary<T1, T2>
+    where T1 : notnull
+    where T2 : notnull
+{
+    private readonly Dictionary<T1, T2> forward;
+    private readonly Dictionary<T2, T1> reverse;
+
+    public Bimap()
+    {
+        forward = new();
+        reverse = new();
+    }
+
+    public Bimap(Bimap<T1, T2> other)
+    {
+        forward = new Dictionary<T1, T2>(other.forward);
+        reverse = new Dictionary<T2, T1>(other.reverse);
+    }
+
+    private Bimap(Dictionary<T1, T2> forward, Dictionary<T2, T1> reverse)
+    {
+        this.forward = forward;
+        this.reverse = reverse;
+    }
+
+    public Bimap<T2, T1> GetReversed()
+    {
+        return new Bimap<T2, T1>(new Dictionary<T2, T1>(reverse), new Dictionary<T1, T2>(forward));
+    }
+
+    public T2 this[T1 key]
+    {
+        get => forward[key];
+
+        set
+        {
+            forward[key] = value;
+            reverse[value] = key;
+        }
+    }
+
+    public T1 GetByValue(T2 value)
+    {
+        if (TryGetKeyFromValue(value, out T1? key))
+            return key;
+        else
+            throw new KeyNotFoundException($"Value {value} not found");
+    }
+
+    public ICollection<T1> Keys => forward.Keys;
+
+    public ICollection<T2> Values => forward.Values;
+
+    public int Count => forward.Count;
+
+    public bool IsReadOnly => ((ICollection<KeyValuePair<T1, T2>>)forward).IsReadOnly;
+
+    public void Add(T1 key, T2 value)
+    {
+        forward.Add(key, value);
+        reverse.Add(value, key);
+    }
+
+    public void Add(KeyValuePair<T1, T2> item)
+    {
+        forward.Add(item.Key, item.Value);
+        reverse.Add(item.Value, item.Key);
+    }
+
+    public void Clear()
+    {
+        forward.Clear();
+        reverse.Clear();
+    }
+
+    public bool Contains(KeyValuePair<T1, T2> item) => ((ICollection<KeyValuePair<T1, T2>>)forward).Contains(item);
+    public bool ContainsKey(T1 key) => forward.ContainsKey(key);
+    public bool ContainsValue(T2 value) => reverse.ContainsKey(value);
+    public void CopyTo(KeyValuePair<T1, T2>[] array, int arrayIndex) => ((ICollection<KeyValuePair<T1, T2>>)forward).CopyTo(array, arrayIndex);
+    public IEnumerator<KeyValuePair<T1, T2>> GetEnumerator() => ((IEnumerable<KeyValuePair<T1, T2>>)forward).GetEnumerator();
+    public bool Remove(T1 key)
+    {
+        if (forward.TryGetValue(key, out T2? value))
+        {
+            forward.Remove(key);
+            reverse.Remove(value);
+            return true;
+        }
+
+        return false;
+    }
+
+    public bool Remove(KeyValuePair<T1, T2> item) => Remove(item.Key);
+    public bool TryGetValue(T1 key, [MaybeNullWhen(false)] out T2 value) => forward.TryGetValue(key, out value);
+    public bool TryGetKeyFromValue(T2 value, [MaybeNullWhen(false)] out T1 key) => reverse.TryGetValue(value, out key);
+
+    IEnumerator IEnumerable.GetEnumerator() => ((IEnumerable)forward).GetEnumerator();
+}
--- day8/Program.cs
+++ day8/Program.cs
@@ -1,5 +1,109 @@
-﻿record Observation(string[] patterns, string[] outputs)
+﻿class Observation
 {
+    string[] patterns;
+    public string[] outputs;
+    Bimap<char, char> mapping = new();
+    public Bimap<int, string> digits = new();
+
+    public Observation(string[] patterns, string[] outputs)
+    {
+        this.patterns = patterns.Select(SortChars).ToArray();
+        this.outputs = outputs.Select(SortChars).ToArray();
+
+        InferMapping();
+    }
+
+    string SortChars(string s) => new string(s.OrderBy(c => c).ToArray());
+
+    void InferMapping()
+    {
+        /*
+            Rules:
+            1. C and F are the only ones that occur in a 2-segment digit
+            2. F occurs in all of the 6-segment digits but C does not
+            -> C and F are identified
+            3. A is the only segment that occurs in a 3-segment digit but not the 2-segment digit
+            -> A is identified
+            4. E and G occur in the 7-segment digit, but not the 2-, 3-, or 4- segment digits
+            5. G occurs in all of the digits with >4 segments but not the 2-, 3-, or 4- segment digits
+            -> E and G are identified
+            -> 6, 0, and 9 are identified
+            6. D is the only segment missing in 0
+            -> D is identified
+            7. By elimination, B is identified
+
+6 ab defg
+0 abc efg
+9 abcd fg
+        */
+
+        digits[1] = patterns.Single(p => p.Length == 2);
+        digits[4] = patterns.Single(p => p.Length == 4);
+        digits[7] = patterns.Single(p => p.Length == 3);
+        digits[8] = patterns.Single(p => p.Length == 7);
+
+        // Rule 1 and 2
+        if (patterns.Where(p => p.Length == 6).All(p => p.Contains(digits[1][0])))
+        {
+            mapping[digits[1][0]] = 'f';
+            mapping[digits[1][1]] = 'c';
+        }
+        else
+        {
+            mapping[digits[1][0]] = 'c';
+            mapping[digits[1][1]] = 'f';
+        }
+
+        // Rule 3
+        mapping[digits[7].Except(digits[1]).Single()] = 'a';
+
+        // Rule 4
+        char[] e_g = digits[8]
+            .Except(patterns.Where(p => p.Length <= 4).SelectMany(p => p)).ToArray();
+        // Rule 5
+        if (patterns.Where(p => p.Length > 4).All(p => p.Contains(e_g[0])))
+        {
+            mapping[e_g[0]] = 'g';
+            mapping[e_g[1]] = 'e';
+        }
+        else
+        {
+            mapping[e_g[0]] = 'e';
+            mapping[e_g[1]] = 'g';
+        }
+
+        digits[6] = patterns.Where(p => p.Length == 6)
+            .Where(p => !p.Contains(mapping.GetByValue('c'))).Single();
+        digits[9] = patterns.Where(p => p.Length == 6)
+            .Where(p => !p.Contains(mapping.GetByValue('e'))).Single();
+        digits[0] = patterns.Where(p => p.Length == 6).Except(digits.Values).Single();
+
+        // Rule 6
+        mapping[digits[8].Except(digits[0]).Single()] = 'd';
+
+        // Rule 7
+        mapping[digits[8].Except(mapping.Keys).Single()] = 'b';
+
+        /*
+        2 a cde g
+        3 a cd fg
+        5 ab d fg
+        */
+        digits[2] = patterns.Where(p => p.Length == 5 && p.Contains(mapping.GetByValue('e'))).Single();
+        digits[5] = patterns.Where(p => p.Length == 5 && p.Contains(mapping.GetByValue('b'))).Single();
+        digits[3] = patterns.Except(digits.Values).Single();
+    }
+
+    public int GetValue()
+    {
+        int value = 0;
+        foreach (string output in outputs)
+        {
+            value *= 10;
+            value += digits.GetByValue(output);
+        }
+        return value;
+    }
 }
 
 class Program
@@ -12,10 +116,47 @@ class Program
             .ToArray();
 
         // Part 1
-        int[] uniquePatterns = { 2, 3, 4, 7 };
+        int[] uniquePatterns = { 2/*1*/, 3/*7*/, 4/*4*/, 7/*8*/ };
         int numUniquePatternsInOutput = observations
                 .Select(obs => obs.outputs.Count(output => uniquePatterns.Contains(output.Length)))
                 .Sum();
         Console.WriteLine(new { numUniquePatternsInOutput });
+
+        // Part 2
+        var total = observations.Select(obs => obs.GetValue()).Sum();
+        Console.WriteLine(new { total });
+
+        /*
+        1   c  f  *
+        4  bcd f  *
+        7 a c  f  *
+        2 a cde g
+        3 a cd fg
+        5 ab d fg
+        6 ab defg
+        0 abc efg
+        9 abcd fg
+        8 abcdefg *
+
+          0:      1:      2:      3:      4:
+         aaaa    ....    aaaa    aaaa    ....
+        b    c  .    c  .    c  .    c  b    c
+        b    c  .    c  .    c  .    c  b    c
+         ....    ....    dddd    dddd    dddd
+        e    f  .    f  e    .  .    f  .    f
+        e    f  .    f  e    .  .    f  .    f
+         gggg    ....    gggg    gggg    ....
+
+          5:      6:      7:      8:      9:
+         aaaa    aaaa    aaaa    aaaa    aaaa
+        b    .  b    .  .    c  b    c  b    c
+        b    .  b    .  .    c  b    c  b    c
+         dddd    dddd    ....    dddd    dddd
+        .    f  e    f  .    f  e    f  .    f
+        .    f  e    f  .    f  e    f  .    f
+         gggg    gggg    ....    gggg    gggg
+                 */
+
+
     }
 }
