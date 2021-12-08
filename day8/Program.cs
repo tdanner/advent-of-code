068b26462c8bf66b9b739aa96382cc565471af52
Tim Danner <tim@danners.us>
2021-12-08 08:24:04 -0600

    Day 8 first half

---
+++ day8/Program.cs
@@ -0,0 +1,21 @@
+﻿record Observation(string[] patterns, string[] outputs)
+{
+}
+
+class Program
+{
+    static void Main()
+    {
+        Observation[] observations = File.ReadAllLines("input.txt")
+            .Select(line => line.Split('|', StringSplitOptions.TrimEntries))
+            .Select(splits => new Observation(splits[0].Split(' '), splits[1].Split(' ')))
+            .ToArray();
+
+        // Part 1
+        int[] uniquePatterns = { 2, 3, 4, 7 };
+        int numUniquePatternsInOutput = observations
+                .Select(obs => obs.outputs.Count(output => uniquePatterns.Contains(output.Length)))
+                .Sum();
+        Console.WriteLine(new { numUniquePatternsInOutput });
+    }
+}
