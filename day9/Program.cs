b6c2a659f0641c0f9935f59cfca40769508380c3
Tim Danner <tim@danners.us>
2021-12-09 08:50:59 -0600

    Day 9 second half

--- day9/Program.cs
+++ day9/Program.cs
@@ -43,3 +43,40 @@ for (int x = 1; x < width - 1; x++)
 }
 
 Console.WriteLine(new { risk });
+
+// Part 2
+int[,] basinMap = new int[width, length];
+void MarkBasin(int basin, int x, int y)
+{
+    if (basinMap[x, y] != 0 || heights[x, y] >= 9)
+        return;
+
+    basinMap[x, y] = basin;
+
+    MarkBasin(basin, x - 1, y);
+    MarkBasin(basin, x + 1, y);
+    MarkBasin(basin, x, y - 1);
+    MarkBasin(basin, x, y + 1);
+}
+
+for (int x = 1; x < width - 1; x++)
+{
+    for (int y = 1; y < length - 1; y++)
+    {
+        MarkBasin(y * length + x, x, y);
+    }
+}
+
+int[] basinSizes = new int[length * width];
+for (int x = 1; x < width - 1; x++)
+{
+    for (int y = 1; y < length - 1; y++)
+    {
+        basinSizes[basinMap[x, y]]++;
+    }
+}
+
+basinSizes[0] = 0; // The zero basin is a count of all points not part of any basin
+
+Array.Sort(basinSizes);
+Console.WriteLine(new { basinProduct = basinSizes[^3..].Aggregate((a, b) => a * b) });
