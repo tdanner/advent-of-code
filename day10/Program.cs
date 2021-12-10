d44126283aac66e86fd3670c8e83e23f39fefa24
Tim Danner <tim@danners.us>
2021-12-10 08:49:01 -0600

    Day 10 first half

---
+++ day10/Program.cs
@@ -0,0 +1,38 @@
+﻿var lines = File.ReadAllLines("input.txt");
+Dictionary<char, char> closers = new()
+{
+    ['('] = ')',
+    ['['] = ']',
+    ['{'] = '}',
+    ['<'] = '>'
+};
+
+Dictionary<char, int> errorValues = new()
+{
+    [')'] = 3,
+    [']'] = 57,
+    ['}'] = 1197,
+    ['>'] = 25137
+};
+
+var score = 0;
+foreach (var line in lines)
+{
+    Stack<char> pending = new();
+    foreach (var c in line)
+        if (closers.TryGetValue(c, out var closer))
+        {
+            pending.Push(closer);
+        }
+        else
+        {
+            var expected = pending.Pop();
+            if (c != expected)
+            {
+                score += errorValues[c];
+                break;
+            }
+        }
+}
+
+Console.WriteLine(new { score });
