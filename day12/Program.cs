04368d9ae5feb10d434372fe59b928e6afda8ee6
Tim Danner <tim@danners.us>
2021-12-12 21:42:02 -0600

    Day 12 first half

---
+++ day12/Program.cs
@@ -0,0 +1,67 @@
+﻿using System.Diagnostics;
+
+record cons(string car, cons? cdr)
+{
+    public bool Contains(string value)
+    {
+        return car == value || (cdr?.Contains(value) ?? false);
+    }
+
+    // Reversed!
+    public override string ToString()
+    {
+        return cdr == null ? car : $"{cdr},{car}";
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // Parse input
+        const string start = "start";
+        const string end = "end";
+        string[] lines = File.ReadAllLines("input.txt");
+        var edges = lines.Select(line => (line[..line.IndexOf('-')], line[(line.IndexOf('-') + 1)..]));
+        List<(string, string)> map = edges.Union(edges.Where(edge => edge.Item1 != start && edge.Item2 != end)
+                                                      .Select(edge => (edge.Item2, edge.Item1)))
+                                          .ToList();
+
+        bool IsBig(string cave) => char.IsUpper(cave[0]);
+
+        Debug.Assert(!map.Where(edge => IsBig(edge.Item1) && IsBig(edge.Item2)).Any());
+
+        // Part 1
+        IEnumerable<cons> FindPaths(cons prefix)
+        {
+            foreach (var (here, nextCave) in map.Where(edge => edge.Item1 == prefix.car))
+            {
+                if (nextCave == end)
+                {
+                    yield return new cons(nextCave, prefix);
+                }
+                else if (IsBig(nextCave) || !prefix.Contains(nextCave))
+                {
+                    foreach (var path in FindPaths(new cons(nextCave, prefix)))
+                    {
+                        yield return path;
+                    }
+                }
+            }
+        }
+
+        var paths = FindPaths(new cons(start, null)).ToList();
+        // paths.ForEach(Console.WriteLine);
+        Console.WriteLine(new { paths.Count });
+    }
+
+    private static void PrintPath(cons path)
+    {
+        if (path.cdr != null)
+        {
+            PrintPath(path.cdr);
+            Console.Write(",");
+        }
+        Console.Write(path.car);
+    }
+}
