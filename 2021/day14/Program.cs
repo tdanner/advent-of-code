837da320a5781faa90d8459615a86139755b3c6d
Tim Danner <tim@danners.us>
2021-12-14 14:00:25 -0600

    Fix reference paths

--- 2021/day14/Program.cs
+++ 2021/day14/Program.cs
@@ -4,7 +4,7 @@ string[] lines = File.ReadAllLines("input.txt");
 string template = lines[0];
 Dictionary<(char, char), char> rules = lines[2..].ToDictionary(line => (line[0], line[1]), line => line[6]);
 
-Counter<(char, char)> pairCounts = new();
+Counter<(char a, char b)> pairCounts = new();
 for (int pos = 0; pos < template.Length - 1; pos++)
 {
     pairCounts[(template[pos], template[pos + 1])]++;
@@ -20,8 +20,8 @@ for (int step = 1; step <= 40; step++)
     {
         char toInsert = rules[pairCount.Key];
         charCounts[toInsert] += pairCount.Value;
-        next[(pairCount.Key.Item1, toInsert)] += pairCount.Value;
-        next[(toInsert, pairCount.Key.Item2)] += pairCount.Value;
+        next[(pairCount.Key.a, toInsert)] += pairCount.Value;
+        next[(toInsert, pairCount.Key.b)] += pairCount.Value;
     }
 
     pairCounts = next;
