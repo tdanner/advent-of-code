e095cf2dbf8f0e22db963586160e7c2d75da554e
Tim Danner <tim@danners.us>
2021-12-14 10:16:31 -0600

    Day 14 second half

--- day14/Program.cs
+++ day14/Program.cs
@@ -1,11 +1,17 @@
-﻿string[] lines = File.ReadAllLines("example.txt");
+﻿string[] lines = File.ReadAllLines("input.txt");
 string template = lines[0];
 Dictionary<(char, char), char> rules = lines[2..].ToDictionary(line => (line[0], line[1]), line => line[6]);
 
 Counter<(char, char)> pairCounts = new();
 for (int pos = 0; pos < template.Length - 1; pos++)
 {
-    pairCounts[(template[pos], template[pos + 1])] = 1;
+    pairCounts[(template[pos], template[pos + 1])]++;
+}
+
+Counter<char> charCounts = new();
+foreach (char c in template)
+{
+    charCounts[c]++;
 }
 
 for (int step = 1; step <= 40; step++)
@@ -14,25 +20,12 @@ for (int step = 1; step <= 40; step++)
     foreach (var pairCount in pairCounts)
     {
         char toInsert = rules[pairCount.Key];
+        charCounts[toInsert] += pairCount.Value;
         next[(pairCount.Key.Item1, toInsert)] += pairCount.Value;
         next[(toInsert, pairCount.Key.Item2)] += pairCount.Value;
     }
 
     pairCounts = next;
-}
-
-Counter<char> charCounts = new();
-foreach (var pair in pairCounts)
-{
-    charCounts[pair.Key.Item1] += pair.Value;
-    charCounts[pair.Key.Item2] += pair.Value;
-}
-// Now all characters have been double-counted except the first and last ones of the template
-charCounts[template[0]]++;
-charCounts[template[^1]]++;
-foreach (char c in charCounts.Keys)
-{
-    charCounts[c] /= 2;
 }
 
 long mostFrequent = charCounts.Values.Max();
