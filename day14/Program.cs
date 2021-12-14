411c88e4a677d3d33d1888e643ed875c48d4a576
Tim Danner <tim@danners.us>
2021-12-14 08:29:37 -0600

    Day 14 first half

---
+++ day14/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+
+string[] lines = File.ReadAllLines("input.txt");
+string template = lines[0];
+Dictionary<string, char> rules = lines[2..].ToDictionary(line => line[0..2], line => line[6]);
+
+for (int step = 1; step <= 10; step++)
+{
+    StringBuilder polymer = new();
+    for (int pos = 0; pos < template.Length - 1; pos++)
+    {
+        polymer.Append(template[pos]);
+        polymer.Append(rules[template[pos..(pos + 2)]]);
+    }
+    polymer.Append(template[^1]);
+    template = polymer.ToString();
+    Console.WriteLine($"After step {step} polymer length is {polymer.Length}");
+    // Console.WriteLine(template);
+}
+
+var histogram = template.GroupBy(c => c).OrderBy(g => g.Count()).ToList();
+int mostFrequent = histogram[^1].Count();
+int leastFrequent = histogram[0].Count();
+Console.WriteLine(new { answer = mostFrequent - leastFrequent });
