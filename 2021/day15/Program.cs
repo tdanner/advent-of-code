6418fe2b5d2e21a7d9fc12768781a96efbf9c47b
Tim Danner <tim@danners.us>
2021-12-15 10:31:20 -0600

    Day 15 second half

--- 2021/day15/Program.cs
+++ 2021/day15/Program.cs
@@ -1,45 +1,97 @@
-﻿class Program
+﻿using System.Diagnostics;
+
+internal class Program
 {
-    static void Main()
+    private static void Main()
     {
-        var lines = File.ReadAllLines("input.txt");
-        int[,] map = new int[lines.Length, lines[0].Length];
-        List<Point> q = new();
-        Dictionary<Point, int> dist = new();
-        Dictionary<Point, Point?> prev = new();
-        for (int y = 0; y < lines.Length; y++)
+        string[] lines = File.ReadAllLines("input.txt");
+        int tileSize = lines.Length;
+        Debug.Assert(lines[0].Length == tileSize);
+        int[,] tile = new int[tileSize, tileSize];
+        int size = tileSize * 5;
+        int[,] map = new int[size, size];
+        for (int y = 0; y < tileSize; y++)
         {
-            for (int x = 0; x < lines[0].Length; x++)
+            for (int x = 0; x < tileSize; x++)
             {
-                var p = new Point(x, y);
-                q.Add(p);
-                dist[p] = int.MaxValue;
-                prev[p] = null;
-                map[y, x] = lines[y][x] - '0';
+                tile[y, x] = lines[y][x] - '0';
             }
         }
+
+        for (int repeatY = 0; repeatY < 5; repeatY++)
+        {
+            for (int repeatX = 0; repeatX < 5; repeatX++)
+            {
+                for (int y = 0; y < tileSize; y++)
+                {
+                    for (int x = 0; x < tileSize; x++)
+                    {
+                        map[y + (tileSize * repeatY), x + (tileSize * repeatX)] =
+                            ((tile[y, x] + repeatX + repeatY - 1) % 9) + 1;
+                    }
+                }
+            }
+        }
+
+        PriorityQueue<Point, int> q = new();
+        Dictionary<Point, int> dist = new();
+        //Dictionary<Point, Point?> prev = new();
+        for (int y = 0; y < map.GetLength(1); y++)
+        {
+            for (int x = 0; x < map.GetLength(0); x++)
+            {
+                var p = new Point(x, y);
+                dist[p] = int.MaxValue;
+                //prev[p] = null;
+            }
+        }
+
         dist[new Point(0, 0)] = 0;
+        q.Enqueue(new Point(0, 0), 0);
 
         IEnumerable<Point> Neighbors(Point p)
         {
-            if (p.y > 0) yield return new Point(p.x, p.y - 1);
-            if (p.x < map.GetLength(1) - 1) yield return new Point(p.x + 1, p.y);
-            if (p.y < map.GetLength(0) - 1) yield return new Point(p.x, p.y + 1);
-            if (p.x > 0) yield return new Point(p.x - 1, p.y);
+            if (p.Y > 0)
+            {
+                yield return new Point(p.X, p.Y - 1);
+            }
+
+            if (p.X < map.GetLength(1) - 1)
+            {
+                yield return new Point(p.X + 1, p.Y);
+            }
+
+            if (p.Y < map.GetLength(0) - 1)
+            {
+                yield return new Point(p.X, p.Y + 1);
+            }
+
+            if (p.X > 0)
+            {
+                yield return new Point(p.X - 1, p.Y);
+            }
         }
 
         // Dijkstra!
-        while (q.Any())
+        HashSet<Point> visited = new();
+        var watch = Stopwatch.StartNew();
+        while (q.TryDequeue(out Point? u, out _))
         {
-            Point u = q.MinBy(p => dist[p])!;
-            q.Remove(u);
-            foreach (Point v in Neighbors(u).Where(q.Contains))
+            if (visited.Contains(u))
             {
-                int alt = dist[u] + map[v.y, v.x];
+                continue;
+            }
+
+            visited.Add(u);
+
+            foreach (Point v in Neighbors(u).Where(n => dist[n] == int.MaxValue))
+            {
+                int alt = dist[u] + map[v.Y, v.X];
                 if (alt < dist[v])
                 {
                     dist[v] = alt;
-                    prev[v] = u;
+                    q.Enqueue(v, alt);
+                    //prev[v] = u;
                 }
             }
         }
@@ -52,11 +104,8 @@
         //     here = prev[here];
         // }
         // Console.WriteLine(string.Join(',', path));
-        Console.WriteLine(new
-        {
-            lowestTotalRisk = dist[new Point(map.GetLength(1) - 1, map.GetLength(0) - 1)]
-        });
+        Console.WriteLine(new { lowestTotalRisk = dist[new Point(map.GetLength(1) - 1, map.GetLength(0) - 1)] });
     }
 }
 
-record Point(int x, int y);
+internal record Point(int X, int Y);
