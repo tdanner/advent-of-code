40445bdcfc741839ee5744d58670a81b0292a4c7
Tim Danner <tim@danners.us>
2021-12-20 00:39:33 -0600

    Day 20 complete

--- 2021/day20/Program.cs
+++ 2021/day20/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System.Diagnostics;
 
-string[] lines = File.ReadAllLines("example.txt");
+string[] lines = File.ReadAllLines("input.txt");
 string algorithm = lines[0];
 Debug.Assert(algorithm.Length == 512);
 int height = lines.Length - 2;
@@ -45,61 +45,13 @@ char Enhanced(Func<int, int, char> image, int y, int x, int numTimes)
     return c2;
 }
 
-Func<int, int, char> Enhance(Func<int, int, char> image, int numTimes)
+int count = 0;
+for (int y = -50; y < height + 50; y++)
 {
-    return (y, x) =>
+    for (int x = -50; x < width + 50; x++)
     {
-        if (memo.TryGetValue((y, x, numTimes), out char c)) return c;
-        if (numTimes == 0)
-        {
-            return image(y, x);
-        }
-
-        int index = 0;
-        for (int sy = -1; sy <= 1; sy++)
-        {
-            for (int sx = -1; sx <= 1; sx++)
-            {
-                index <<= 1;
-                index |= image(y + sy, x + sx) == '#' ? 1 : 0;
-            }
-        }
-
-        char c2 = algorithm[index];
-        memo[(y, x, numTimes)] = c2;
-        return c2;
-    };
-}
-
-Console.WriteLine("Loaded image:");
-Print(image, 0, height, 0, width);
-
-image = Enhance(image, 50);
-
-Console.WriteLine(new { pixelCount = CountPixelsOn(image, -50, height + 50, -50, width + 50) });
-
-void Print(Func<int, int, char> image, int yMin, int yMax, int xMin, int xMax)
-{
-    for (int y = yMin; y < yMax; y++)
-    {
-        for (int x = xMin; x < xMax; x++)
-        {
-            Console.Write(image(y, x));
-        }
-        Console.WriteLine();
+        if (Enhanced(image, y, x, 50) == '#') count++;
     }
 }
 
-int CountPixelsOn(Func<int, int, char> image, int yMin, int yMax, int xMin, int xMax)
-{
-    int count = 0;
-    for (int y = yMin; y < yMax; y++)
-    {
-        for (int x = xMin; x < xMax; x++)
-        {
-            if (image(y, x) == '#') count++;
-        }
-    }
-
-    return count;
-}
+Console.WriteLine(new { count });
