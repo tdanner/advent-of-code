80dbd20baca507b58a6c57825d83815b14c8b43f
Tim Danner <tim@danners.us>
2021-12-24 16:38:46 -0600

    Day 21 second half

--- 2021/day21/Program.cs
+++ 2021/day21/Program.cs
@@ -1,12 +1,14 @@
 ﻿using System.Diagnostics;
 using System.Text.RegularExpressions;
+using TimDanner.Utils;
 
 class Program
 {
+    static Dictionary<int, int> playerPositions = new();
+
     static void Main()
     {
         string[] lines = File.ReadAllLines("input.txt");
-        Dictionary<int, int> playerPositions = new();
         foreach (string line in lines)
         {
             Match m = Regex.Match(line, @"^Player (\d+) starting position: (\d+)$");
@@ -14,6 +16,12 @@ class Program
             playerPositions[int.Parse(m.Groups[1].Value)] = int.Parse(m.Groups[2].Value);
         }
 
+        // FirstPart();
+        SecondPart();
+    }
+
+    static void FirstPart()
+    {
         Die die = new Die();
         Dictionary<int, int> playerScores = new();
         foreach (int player in playerPositions.Keys)
@@ -35,8 +43,64 @@ class Program
 
         Console.WriteLine(playerScores.Values.Min() * die.NumRolls);
     }
-}
 
+    static void SecondPart()
+    {
+        Counter<Universe> multiverse = new();
+        multiverse[new Universe(new Player(playerPositions[1], 0),
+                                new Player(playerPositions[2], 0),
+                                null)] = 1;
+
+        bool player1sTurn = true;
+        while (multiverse.Keys.Any(uni => uni.winner == null))
+        {
+            Console.WriteLine($"num universes: {multiverse.Keys.Count()}");
+            Counter<Universe> nextTurn = new();
+            foreach (Universe uni in multiverse.Keys)
+            {
+                if (uni.winner != null)
+                {
+                    nextTurn[uni] += multiverse[uni];
+                    continue;
+                }
+
+                for (int roll1 = 1; roll1 <= 3; roll1++)
+                {
+                    for (int roll2 = 1; roll2 <= 3; roll2++)
+                    {
+                        for (int roll3 = 1; roll3 <= 3; roll3++)
+                        {
+                            int move = roll1 + roll2 + roll3;
+                            Player pBefore = player1sTurn ? uni.p1 : uni.p2;
+                            int position = (pBefore.position - 1 + move) % 10 + 1;
+                            Player pAfter = new(position, pBefore.score + position);
+                            int? winner = null;
+                            if (pAfter.score >= 21)
+                                winner = player1sTurn ? 1 : 2;
+
+                            Universe uniAfter = player1sTurn ?
+                                new Universe(pAfter, uni.p2, winner) :
+                                new Universe(uni.p1, pAfter, winner);
+
+                            nextTurn[uniAfter] += multiverse[uni];
+                        }
+                    }
+                }
+            }
+            // 414036821918489
+            // 217503477215922
+            // 
+            multiverse = nextTurn;
+            player1sTurn = !player1sTurn;
+        }
+
+        Console.WriteLine(new
+        {
+            player1wins = multiverse.Where(uni => uni.Key.winner == 1).Sum(uni => uni.Value),
+            player2Wins = multiverse.Where(uni => uni.Key.winner == 2).Sum(uni => uni.Value)
+        });
+    }
+}
 class Die
 {
     int nextRoll = 0;
@@ -48,3 +112,6 @@ class Die
         return ++nextRoll;
     }
 }
+
+record Player(int position, int score);
+record Universe(Player p1, Player p2, int? winner);
