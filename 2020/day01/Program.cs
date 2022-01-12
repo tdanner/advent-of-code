a29c85789b038e0721b01bc7ff115f08ea7dc25d
Tim Danner <tim@danners.us>
2022-01-11 23:05:21 -0600

    Day 1 of 2020

---
+++ 2020/day01/Program.cs
@@ -0,0 +1,15 @@
+﻿var expenses = File.ReadAllLines("input.txt").Select(long.Parse).ToArray();
+foreach (var e1 in expenses)
+{
+    foreach (var e2 in expenses)
+    {
+        foreach (var e3 in expenses)
+        {
+            if (e1 + e2 + e3 == 2020)
+            {
+                Console.WriteLine(e1 * e2 * e3);
+                break;
+            }
+        }
+    }
+}
