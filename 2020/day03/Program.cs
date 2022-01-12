0bc915aa009a08d636856b6c64e87e4b0715d725
Tim Danner <tim@danners.us>
2022-01-12 16:52:00 -0600

    2020 day 3 part 2

--- 2020/day03/Program.cs
+++ 2020/day03/Program.cs
@@ -2,15 +2,29 @@
 int height = lines.Length;
 int width = lines[0].Length;
 var map = (int x, int y) => lines[y][x % width];
-int vx = 3, vy = 1;
-int x = 0, y = 0;
-int trees = 0;
-while (y < height)
-{
-    if (map(x, y) == '#')
-        trees++;
-    x += vx;
-    y += vy;
 
+int treeCount((int, int) slope)
+{
+    var (vx, vy) = slope;
+    int x = 0, y = 0;
+    int trees = 0;
+    while (y < height)
+    {
+        if (map(x, y) == '#')
+            trees++;
+        x += vx;
+        y += vy;
+
+    }
+    return trees;
 }
-Console.WriteLine(trees);
+
+(int, int)[] slopes = new[] {
+    (1,1),
+    (3,1),
+    (5,1),
+    (7,1),
+    (1,2),
+};
+
+Console.WriteLine(slopes.Select(treeCount).Aggregate(1L, (a, b) => a * b));
