1eadac5dc8bcb5cb35d14408eeb731b9b0299e74
Tim Danner <tim@danners.us>
2022-01-12 09:54:26 -0600

    2020 day 2 part 1

---
+++ 2020/day02/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.RegularExpressions;
+
+var passwords = File.ReadAllLines("input.txt")
+            .Select(l => Regex.Match(l, @"^(\d+)-(\d+) ([a-z]): ([a-z]+)$"))
+            .Select(m => new Password(int.Parse(m.Groups[1].Value), int.Parse(m.Groups[2].Value), m.Groups[3].Value[0], m.Groups[4].Value))
+            .ToArray();
+
+Console.WriteLine(passwords.Count(p => p.IsValid()));
+
+record Password(int Min, int Max, char Required, string Actual)
+{
+    public bool IsValid()
+    {
+        int c = Actual.Count(c => c == Required);
+        return c >= Min && c <= Max;
+    }
+}
