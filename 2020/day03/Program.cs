987536223b2a688ebf3de0b2c422adbc0dcc73f0
Tim Danner <tim@danners.us>
2022-01-12 16:43:57 -0600

    2020 day 3 part 1

---
+++ 2020/day03/Program.cs
@@ -0,0 +1,16 @@
+﻿var lines = File.ReadAllLines("input.txt");
+int height = lines.Length;
+int width = lines[0].Length;
+var map = (int x, int y) => lines[y][x % width];
+int vx = 3, vy = 1;
+int x = 0, y = 0;
+int trees = 0;
+while (y < height)
+{
+    if (map(x, y) == '#')
+        trees++;
+    x += vx;
+    y += vy;
+
+}
+Console.WriteLine(trees);
