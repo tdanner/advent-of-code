0be1d26f08a5f285fd8f096a6f701c9c34dd0d6a
Tim Danner <tim@danners.us>
2022-01-12 10:03:04 -0600

    2020 day 2 part 2

--- 2020/day02/Program.cs
+++ 2020/day02/Program.cs
@@ -5,13 +5,19 @@ var passwords = File.ReadAllLines("input.txt")
             .Select(m => new Password(int.Parse(m.Groups[1].Value), int.Parse(m.Groups[2].Value), m.Groups[3].Value[0], m.Groups[4].Value))
             .ToArray();
 
-Console.WriteLine(passwords.Count(p => p.IsValid()));
+Console.WriteLine(passwords.Count(p => p.IsValidPart1()));
+Console.WriteLine(passwords.Count(p => p.IsValidPart2()));
 
 record Password(int Min, int Max, char Required, string Actual)
 {
-    public bool IsValid()
+    public bool IsValidPart1()
     {
         int c = Actual.Count(c => c == Required);
         return c >= Min && c <= Max;
+    }
+
+    public bool IsValidPart2()
+    {
+        return (Actual[Min - 1] == Required) ^ (Actual[Max - 1] == Required);
     }
 }
