31f1caca60906ce27b3aa660d1fdfe35f1068a44
Tim Danner <tim@danners.us>
2022-01-13 11:35:26 -0600

    2020 day 4 part 2

--- 2020/day04/main.go
+++ 2020/day04/main.go
@@ -5,6 +5,8 @@ import (
 	"io"
 	"log"
 	"os"
+	"regexp"
+	"strconv"
 	"strings"
 )
 
@@ -19,8 +21,12 @@ func main() {
 	}
 	valids := 0
 	for _, v := range strings.Split(string(data), "\n\n") {
-		if isValid(parsePassport(v)) {
+		passport := parsePassport(v)
+		delete(passport, "cid")
+		valid := isValid(passport)
+		if valid {
 			valids++
+			fmt.Println(passport)
 		}
 	}
 	fmt.Println(valids)
@@ -35,7 +41,8 @@ func parsePassport(passport string) (values map[string]string) {
 	return
 }
 
-var requiredFields = [...]string{"byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"}
+var requiredFields = []string{"byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"}
+var eyeColors = []string{"amb", "blu", "brn", "gry", "grn", "hzl", "oth"}
 
 func isValid(passport map[string]string) bool {
 	for _, v := range requiredFields {
@@ -43,5 +50,63 @@ func isValid(passport map[string]string) bool {
 			return false
 		}
 	}
+
+	byr, err := strconv.Atoi(passport["byr"])
+	if err != nil || byr < 1920 || byr > 2002 {
+		return false
+	}
+
+	iyr, err := strconv.Atoi(passport["iyr"])
+	if err != nil || iyr < 2010 || iyr > 2020 {
+		return false
+	}
+
+	eyr, err := strconv.Atoi(passport["eyr"])
+	if err != nil || eyr < 2020 || eyr > 2030 {
+		return false
+	}
+
+	hgt := passport["hgt"]
+	height, err := strconv.Atoi(hgt[:len(hgt)-2])
+	if err != nil {
+		return false
+	}
+	hUnit := hgt[len(hgt)-2:]
+	if hUnit == "cm" {
+		if height < 150 || height > 193 {
+			return false
+		}
+	} else if hUnit == "in" {
+		if height < 59 || height > 76 {
+			return false
+		}
+	} else {
+		return false
+	}
+
+	hcl := passport["hcl"]
+	if matched, err := regexp.MatchString(`^#[0-9a-f]{6}$`, hcl); !matched || err != nil {
+		return false
+	}
+
+	ecl := passport["ecl"]
+	if !contains(eyeColors, ecl) {
+		return false
+	}
+
+	pid := passport["pid"]
+	if matched, err := regexp.MatchString(`^[0-9]{9}$`, pid); !matched || err != nil {
+		return false
+	}
+
 	return true
+}
+
+func contains(arr []string, str string) bool {
+	for _, v := range arr {
+		if v == str {
+			return true
+		}
+	}
+	return false
 }
