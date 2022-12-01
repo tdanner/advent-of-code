f1c21cdc7c0a4069c063d9ae54e8b2b78f8e87f8
Tim Danner <tim@danners.us>
2022-11-30 23:05:10 -0600

    2022 day 1 part 1

---
+++ 2022/day01/Program.cs
@@ -0,0 +1,4 @@
+﻿string input = File.ReadAllText("input1.txt");
+string[] elves = input.Split("\n\n");
+int max = elves.Select(elf => elf.Trim().Split("\n").Select(int.Parse).Sum()).Max();
+Console.WriteLine(max);
