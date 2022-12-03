d9e8766a8238adad1f83696df8fb52b7ad40be4e
Tim Danner <tim@danners.us>
2022-12-02 23:33:33 -0600

    2022 day 3 part 1

---
+++ 2022/day03/Program.cs
@@ -0,0 +1,26 @@
+﻿var lines = File.ReadAllLines("input.txt");
+int total = lines.Select(FindCommonChar).Select(GetPriority).Sum();
+Console.WriteLine(total);
+
+char FindCommonChar(string line)
+{
+    int mid = line.Length / 2;
+    for (int i = 0; i < mid; i++)
+    {
+        for (int j = mid; j < line.Length; j++)
+        {
+            if (line[i] == line[j])
+            {
+                return line[i];
+            }
+        }
+    }
+    throw new Exception($"No common char found in {line}");
+}
+
+int GetPriority(char c) => c switch
+{
+    >= 'a' and <= 'z' => (c - 'a') + 1,
+    >= 'A' and <= 'Z' => (c - 'A') + 27,
+    _ => throw new Exception($"Unexpected char {c}")
+};
