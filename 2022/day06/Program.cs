81ec668ee2c6152ad54ada85b83c493fdd6e9a65
Tim Danner <tim@danners.us>
2022-12-05 23:06:07 -0600

    2022 day 6 part 1

---
+++ 2022/day06/Program.cs
@@ -0,0 +1,10 @@
+﻿var input = File.ReadAllText("input.txt");
+for (int i = 3; i < input.Length; i++)
+{
+    char a = input[i - 3], b = input[i - 2], c = input[i - 1], d = input[i];
+    if (a != b && a != c && a != d && b != c && b != d && c != d)
+    {
+        Console.WriteLine(i + 1);
+        break;
+    }
+}
