12fcc7c8a5e2e767e8bbe07526692266375db6b6
Tim Danner <tim@danners.us>
2022-12-05 23:11:54 -0600

    2022 day 6 part 2

--- 2022/day06/Program.cs
+++ 2022/day06/Program.cs
@@ -4,7 +4,17 @@ for (int i = 3; i < input.Length; i++)
     char a = input[i - 3], b = input[i - 2], c = input[i - 1], d = input[i];
     if (a != b && a != c && a != d && b != c && b != d && c != d)
     {
-        Console.WriteLine(i + 1);
+        Console.WriteLine("start of packet: " + (i + 1));
+        break;
+    }
+}
+
+for (int i = 13; i < input.Length; i++)
+{
+    var chunk = input[(i - 13)..(i + 1)];
+    if (chunk.Distinct().Count() == 14)
+    {
+        Console.WriteLine("start of message: " + (i + 1));
         break;
     }
 }
