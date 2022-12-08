5870cf9e6472c6ff0c6f53c67ab3c8d418eb7e23
Tim Danner <tim@danners.us>
2022-12-08 00:47:32 -0600

    2022 day 8 part 1

---
+++ 2022/day08/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Text;
+
+var lines = File.ReadAllLines("input.txt");
+var visible = new bool[lines.Length, lines[0].Length];
+
+for (int y = 0; y < lines.Length; y++)
+{
+    SetVisible(0, y, 1, 0);
+    SetVisible(lines[0].Length - 1, y, -1, 0);
+}
+for (int x = 0; x < lines[0].Length; x++)
+{
+    SetVisible(x, 0, 0, 1);
+    SetVisible(x, lines.Length - 1, 0, -1);
+}
+
+int part1 = 0;
+var buffer = new StringBuilder();
+for (int y = 0; y < lines.Length; y++)
+{
+    for (int x = 0; x < lines[0].Length; x++)
+    {
+        if (visible[y, x])
+        {
+            buffer.Append('*');
+            part1++;
+        }
+        else
+        {
+            buffer.Append(' ');
+        }
+    }
+    buffer.AppendLine();
+}
+
+Console.WriteLine(buffer.ToString());
+
+Console.WriteLine($"Part 1: {part1}");
+
+
+void SetVisible(int x, int y, int dx, int dy)
+{
+    int minHeight = -1;
+    while (x >= 0 && x < lines[0].Length &&
+           y >= 0 && y < lines.Length)
+    {
+        int height = lines[y][x] - '0';
+        if (height > minHeight)
+        {
+            visible[y, x] = true;
+        }
+        minHeight = Math.Max(height, minHeight);
+        x += dx;
+        y += dy;
+    }
+}
