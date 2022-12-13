fba69b84289049b3f019fcdb76cdc791ec936c22
Tim Danner <tim@danners.us>
2022-12-12 23:40:54 -0600

    2022 day 12 part 2

--- 2022/day12/Program.cs
+++ 2022/day12/Program.cs
@@ -28,18 +28,20 @@ for (int y = 0; y < h; y++)
     }
 }
 
-dist[start] = 0;
-
+dist[end] = 0;
+List<Pt> candidates = new();
 while (q.Count > 0)
 {
     Pt u = q.MinBy(p => dist[p]);
-    if (u == end)
+    if (z[u.y, u.x] == 0)
     {
-        break;
+        candidates.Add(u);
     }
     q.Remove(u);
     foreach (var v in neighbors(u).Where(q.Contains))
     {
+        if (dist[u] == int.MaxValue)
+            continue;
         int alt = dist[u] + 1;
         if (alt < dist[v])
         {
@@ -50,21 +52,22 @@ while (q.Count > 0)
 }
 
 Console.WriteLine($"Part 1: {dist[end]}");
+Console.WriteLine($"Part 2: {candidates.Select(c => dist[c]).Min()}");
 
 IEnumerable<Pt> neighbors(Pt a)
 {
-    var maxz = z[a.y, a.x] + 1;
+    var minz = z[a.y, a.x] - 1;
     // up
-    if (a.y > 0 && z[a.y - 1, a.x] <= maxz)
+    if (a.y > 0 && z[a.y - 1, a.x] >= minz)
         yield return new Pt(a.x, a.y - 1);
     // down
-    if (a.y < h - 1 && z[a.y + 1, a.x] <= maxz)
+    if (a.y < h - 1 && z[a.y + 1, a.x] >= minz)
         yield return new Pt(a.x, a.y + 1);
     // left
-    if (a.x > 0 && z[a.y, a.x - 1] <= maxz)
+    if (a.x > 0 && z[a.y, a.x - 1] >= minz)
         yield return new Pt(a.x - 1, a.y);
     // right
-    if (a.x < w - 1 && z[a.y, a.x + 1] <= maxz)
+    if (a.x < w - 1 && z[a.y, a.x + 1] >= minz)
         yield return new Pt(a.x + 1, a.y);
 }
 
