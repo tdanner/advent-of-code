a21f44aad78937f0f595fd6af3dd6fdf9d83b586
Tim Danner <tim@danners.us>
2022-12-17 08:37:53 -0600

    2022 day 16 part 2 work in progress

--- 2022/day16/Program.cs
+++ 2022/day16/Program.cs
@@ -1,29 +1,50 @@
 ﻿using System.Text.Json;
 using System.Text.RegularExpressions;
 
-internal class Program
+internal static class Program
 {
     static Dictionary<string, int> flowRates = new();
+    static Dictionary<ulong, int> flowRateBits = new();
     static Dictionary<string, string[]> tunnels = new();
+    static Dictionary<ulong, ulong[]> tunnelBits = new();
+    static Dictionary<string, ulong> valveBits = new();
+    static Dictionary<ulong, Dictionary<ulong, ulong>> directionsTo = new();
+    static int numValvesToOpen;
+    static ulong allValvesBits;
 
     private static void Main(string[] args)
     {
         var lines = File.ReadAllLines("input.txt");
         var parser = new Regex(@"Valve ([A-Z][A-Z]) has flow rate=(\d+); tunnels? leads? to valves? ([A-Z, ]+)");
 
+        ulong valveBit = 1;
         foreach (var line in lines)
         {
             var m = parser.Match(line);
             var id = m.Groups[1].Value;
             flowRates[id] = int.Parse(m.Groups[2].Value);
             tunnels[id] = m.Groups[3].Value.Split(", ");
+            valveBits[id] = valveBit;
+            valveBit <<= 1;
         }
         Console.WriteLine(JsonSerializer.Serialize(flowRates));
         Console.WriteLine(JsonSerializer.Serialize(tunnels));
+        numValvesToOpen = flowRates.Values.Count(f => f > 0);
+
+        foreach (ulong valve in flowRateBits.Keys)
+        {
+            directionsTo[valve] = HowToGetTo(valve);
+        }
 
         Console.WriteLine("graph");
         foreach (var (v, ts) in tunnels)
         {
+            tunnelBits[valveBits[v]] = ts.Select(t => valveBits[t]).ToArray();
+            flowRateBits[valveBits[v]] = flowRates[v];
+            if (flowRates[v] > 0)
+            {
+                allValvesBits |= valveBits[v];
+            }
             foreach (var t in ts)
             {
                 string rate = flowRates[v] > 0 ? $" {flowRates[v]}" : "";
@@ -31,26 +52,25 @@ internal class Program
             }
         }
 
-        State start = new State("AA", new List<string>(), 1, 0, 0);
-        DFS(start);
+        // State start = new State("AA", new List<string>(), 1, 0, 0);
+        // DFS(start);
+        ulong aa = valveBits["AA"];
+        State2 start = new State2(aa, aa, 0, 1, 0, 0, 0);
+        DFS2(start);
     }
 
-    static List<string> ShortestPath(string src, string dst)
+    static Dictionary<ulong, ulong> HowToGetTo(ulong dest)
     {
-        var dist = tunnels!.Keys.ToDictionary(k => k, k => int.MaxValue);
-        var prev = new Dictionary<string, string>();
-        var q = new HashSet<string>(tunnels.Keys);
-        dist[src] = 0;
+        var dist = tunnelBits!.Keys.ToDictionary(k => k, k => int.MaxValue);
+        var prev = new Dictionary<ulong, ulong>();
+        var q = new HashSet<ulong>(tunnelBits.Keys);
+        dist[dest] = 0;
 
         while (q.Count > 0)
         {
-            string u = q.MinBy(x => dist[x])!;
-            if (u == dst)
-            {
-                break;
-            }
+            ulong u = q.MinBy(x => dist[x])!;
             q.Remove(u);
-            foreach (var v in tunnels[u].Where(q.Contains))
+            foreach (var v in tunnelBits[u].Where(q.Contains))
             {
                 var alt = dist[u] + 1;
                 if (alt < dist[v])
@@ -61,80 +81,114 @@ internal class Program
             }
         }
 
-        var path = new List<string>();
-        var w = dst;
-        path.Add(w);
-        while (prev.TryGetValue(w, out var v))
-        {
-            w = v;
-            path.Add(w);
-        }
-        path.Reverse();
-        return path;
+        return prev;
     }
 
     static int bestReleased = 0;
     static int pathsSearched = 0;
-    static void DFS(State here)
+
+    static void DFS2(State2 here)
     {
         var options = here.GetNextStates().ToList();
         if (options.Count == 0)
         {
-            int releasing = here.Releasing;
-            for (int t = here.Time; t <= 30; t++)
-            {
-                here = here with { Time = t, Released = here.Released + releasing };
-            }
             pathsSearched++;
             if (here.Released > bestReleased)
             {
                 bestReleased = here.Released;
                 Console.WriteLine($"Got a final state with released {here.Released} and time {here.Time}");
             }
+            if (pathsSearched % 10000000 == 0)
+            {
+                Console.WriteLine($"Searched {pathsSearched} paths; best = {bestReleased}");
+            }
             return;
         }
 
-        options.Sort((s1, s2) => -s1.JustOpened.CompareTo(s2.JustOpened));
         foreach (var next in options)
         {
-            DFS(next);
+            DFS2(next);
         }
     }
 
-    record struct State(string Loc, List<string> Open, int Time, int Released, int JustOpened)
+    record struct State2(ulong YLoc, ulong ELoc, ulong Open, int Time, int Released, ulong YVisited, ulong EVisited)
     {
-        public int Releasing => Open.Select(v => flowRates[v]).Sum();
-        public IEnumerable<State> GetNextStates()
+        public int Releasing
         {
-            var open = Open;
-            // iterate through the valves that are not yet open and return a state for each one
-            // that represents traveling to that valve and opening it
-            foreach (var dest in tunnels.Keys.Where(v => flowRates[v] > 0 && !open.Contains(v)))
+            get
             {
-                var path = ShortestPath(Loc, dest);
-                if (Time + path.Count > 30)
+                int r = 0;
+                for (int i = 0; i < valveBits.Count; i++)
                 {
-                    continue; // not a viable path
+                    if ((Open & ((ulong)1 << i)) != 0)
+                    {
+                        r += flowRateBits[(ulong)1 << i];
+                    }
                 }
-                int releasing = Releasing;
-                int released = Released;
-                int time = Time;
-                foreach (var step in path)
+                return r;
+            }
+        }
+
+        public IEnumerable<ulong> GetReasonableMoves(ulong start, ulong visited)
+        {
+            foreach (ulong next in tunnelBits[start])
+            {
+                if ((next & visited) == 0)
                 {
-                    time++;
-                    released += releasing;
+                    yield return next;
                 }
-                var nowOpen = new List<string>(open);
-                nowOpen.Add(dest);
-                yield return new State(dest, nowOpen, time, released, flowRates[dest]);
+            }
+        }
+
+        public IEnumerable<State2> GetNextStates()
+        {
+            // we are going to iterate through all possible moves. no path planning!
+            // that means cross-product of the number of tunnels Y and E can traverse + open the local valve 😬
+            if (Time == 27)
+            {
+                yield break;
+            }
+            if (Open == allValvesBits)
+            {
+                yield return this with { Time = Time + 1, Released = Released + Releasing };
+                yield break;
+            }
+
+            IEnumerable<ulong> eoptions = GetReasonableMoves(ELoc, EVisited);
+            if (flowRateBits[ELoc] > 0 && ((Open & ELoc) == 0))
+            {
+                eoptions = eoptions.Append(0UL);
+            }
+            IEnumerable<ulong> yoptions = GetReasonableMoves(YLoc, YVisited);
+            if (flowRateBits[YLoc] > 0 && ((Open & YLoc) == 0) && YLoc != ELoc) // if Y and E are on the save valve, only E opens it
+            {
+                yoptions = yoptions.Append(0UL);
+            }
+            int releasing = Releasing;
+            foreach (var enext in eoptions)
+            {
+                foreach (var ynext in yoptions)
+                {
+                    var open = Open;
+                    if (enext == 0UL)
+                    {
+                        open |= ELoc;
+                    }
+                    if (ynext == 0UL)
+                    {
+                        open |= YLoc;
+                    }
+                    yield return new State2(
+                        ynext == 0UL ? YLoc : ynext,
+                        enext == 0UL ? ELoc : enext,
+                        open,
+                        Time + 1,
+                        Released + Releasing,
+                        ynext == 0 ? 0 : YVisited | ynext,
+                        enext == 0 ? 0 : EVisited | enext
+                    );
+                }
             }
         }
     }
 }
-
-// we are going to be searching though path space to find the best terminal state
-// a terminal state has Time=30
-// the best terminal state has the highest Flowed
-// a state transition consists of travelling to another valve and opening it
-// we will use recursion to explore the state space, which means states need to immutable
-// would it be crazy to use a bitmask to represent the open valves? I don't think that would be crazy
