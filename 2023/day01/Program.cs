02510e7d66083885500d69b8e7be4bfc4b11d0d0
Tim Danner <tim@danners.us>
2023-11-30 23:26:08 -0600

    2023 day 01 part 2

--- 2023/day01/Program.cs
+++ 2023/day01/Program.cs
@@ -1,7 +1,26 @@
-﻿var lines = File.ReadAllLines("input1.txt");
+﻿using System.Text.RegularExpressions;
+
+var numbers = new[]{
+    "zeroxxxxxxxx", "one", "two", "three","four", "five", "six", "seven", "eight", "nine"
+};
+
+int ToNum(string v)
+{
+    if (v.Length == 1)
+        return int.Parse(v);
+    var i = Array.IndexOf(numbers, v);
+    if (i < 0)
+        throw new Exception($"didn't find {v}");
+    return i;
+}
+
+var firstDigitRE = new Regex(string.Join("|", numbers) + "|[0-9]");
+var lastDigitRE = new Regex(string.Join("|", numbers) + "|[0-9]", RegexOptions.RightToLeft);
+var lines = File.ReadAllLines("input1.txt");
 var sum = lines.Select(l =>
 {
-    var digits = l.Where(c => char.IsDigit(c)).ToArray();
-    return int.Parse($"{digits[0]}{digits.Last()}");
+    var first = firstDigitRE.Match(l).Value;
+    var last = lastDigitRE.Match(l).Value;
+    return ToNum(first) * 10 + ToNum(last);
 }).Sum();
 Console.WriteLine(sum);
