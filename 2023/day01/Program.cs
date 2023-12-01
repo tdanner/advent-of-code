d0d6bf05befc85148dfad6a7eee679536e89fb2e
Tim Danner <tim@danners.us>
2023-11-30 23:07:30 -0600

    2023 day 01 part 1

---
+++ 2023/day01/Program.cs
@@ -0,0 +1,7 @@
+﻿var lines = File.ReadAllLines("input1.txt");
+var sum = lines.Select(l =>
+{
+    var digits = l.Where(c => char.IsDigit(c)).ToArray();
+    return int.Parse($"{digits[0]}{digits.Last()}");
+}).Sum();
+Console.WriteLine(sum);
