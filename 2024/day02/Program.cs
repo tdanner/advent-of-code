733e2efeffb83d87a27ff1f8b7ebc3c13c032b10
Tim Danner <tim@danners.us>
2024-12-03 22:52:11 -0600

    2024 day 02 part 2

--- 2024/day02/Program.cs
+++ 2024/day02/Program.cs
@@ -9,6 +9,23 @@
         diffs.All(d => d >= -3 && d <= -1);
 }
 
+bool IsSafeExceptOne(List<int> levels)
+{
+    for (int skip = 0; skip < levels.Count; skip++)
+    {
+        var skipped = new List<int>(levels);
+        skipped.RemoveAt(skip);
+        if (IsSafe(skipped))
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
 var lines = File.ReadAllLines("input.txt");
 var part1 = lines.Select(l => l.Split(" ").Select(int.Parse).ToList()).Where(IsSafe).Count();
 Console.WriteLine("Part 1: " + part1);
+
+var part2 = lines.Select(l => l.Split(" ").Select(int.Parse).ToList()).Where(IsSafeExceptOne).Count();
+Console.WriteLine("Part 2: " + part2);
