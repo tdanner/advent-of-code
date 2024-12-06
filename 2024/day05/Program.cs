ce66e737b7e25e75f1c6a4dc2d43c1d7f0f2c0f6
Tim Danner <tim@danners.us>
2024-12-05 20:32:49 -0600

    2024 day 05 part 2

--- 2024/day05/Program.cs
+++ 2024/day05/Program.cs
@@ -20,7 +20,26 @@ foreach (var line in lines[..blank])
 
 // Console.WriteLine(JsonSerializer.Serialize(rules));
 
-int score = 0;
+bool ruleExists(int first, int second)
+{
+    return rules.GetValueOrDefault(first, empty).Contains(second);
+}
+
+int ruleCompare(int a, int b)
+{
+    if (ruleExists(a, b))
+    {
+        return -1;
+    }
+    if (ruleExists(b, a))
+    {
+        return 1;
+    }
+    return 0;
+}
+
+int part1 = 0;
+int part2 = 0;
 foreach (var line in lines[(blank + 1)..])
 {
     var pages = line.Split(",").Select(int.Parse).Index().ToDictionary(pair => pair.Item, pair => pair.Index);
@@ -49,9 +68,17 @@ foreach (var line in lines[(blank + 1)..])
     {
         var middle = int.Parse(line[(line.Length / 2 - 1)..(line.Length / 2 + 1)]);
         // Console.WriteLine($"{line} is correctly ordered (middle={middle})");
-        score += middle;
+        part1 += middle;
     }
-
+    else
+    {
+        var sorted = pages.Keys.ToList();
+        sorted.Sort(ruleCompare);
+        var middle = sorted[sorted.Count / 2];
+        part2 += middle;
+        // Console.WriteLine($"{line} becomes {string.Join(",", sorted)} (middle={middle})");
+    }
 }
 
-Console.WriteLine($"Part 1: {score}");
+Console.WriteLine($"Part 1: {part1}");
+Console.WriteLine($"Part 2: {part2}");
