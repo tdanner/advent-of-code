fc801f378b7bb5d1308c8eca6e09d3cdfc0b7e89
Tim Danner <tim@danners.us>
2024-12-13 22:18:30 -0600

    2024 day 13 part 1

---
+++ swift/2024/Sources/Day13.swift
@@ -0,0 +1,49 @@
+import Foundation
+
+struct Day13: Day {
+    struct Game {
+        let a, b, prize: Point
+
+        func tokens() -> Int {
+            let bTokens =
+                Double(a.x * prize.y - a.y * prize.x)
+                / Double(a.x * b.y - a.y * b.x)
+            if bTokens.truncatingRemainder(dividingBy: 1) != 0 {
+                return 0
+            }
+            let aTokens = Double(prize.x - Int(bTokens) * b.x) / Double(a.x)
+            if aTokens.truncatingRemainder(dividingBy: 1) != 0 {
+                return 0
+            }
+            return Int(aTokens) * 3 + Int(bTokens)
+        }
+    }
+
+    let games: [Game]
+
+    init(input: String) {
+        let lines = input.split(separator: "\n")
+        let buttonRegex = /Button [AB]: X\+(\d+), Y\+(\d+)/
+        let prizeRegex = /Prize: X=(\d+), Y=(\d+)/
+        var games: [Game] = []
+        for l in stride(from: 0, to: lines.count, by: 3) {
+            let buttonA = lines[l].firstMatch(of: buttonRegex)!
+            let buttonB = lines[l + 1].firstMatch(of: buttonRegex)!
+            let prize = lines[l + 2].firstMatch(of: prizeRegex)!
+            let game = Game(
+                a: Point(Int(buttonA.1)!, Int(buttonA.2)!),
+                b: Point(Int(buttonB.1)!, Int(buttonB.2)!),
+                prize: Point(Int(prize.1)!, Int(prize.2)!))
+            games.append(game)
+        }
+        self.games = games
+    }
+
+    func partOne() -> Int {
+        return games.map({ $0.tokens() }).reduce(0, +)
+    }
+
+    func partTwo() -> Int {
+        return 0
+    }
+}
