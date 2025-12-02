2927544bfb2c0c826499e9a5f4b27bd5e9da1004
Tim Danner <tim@danners.us>
2025-12-02 06:43:03 -0600

    2025 day 02 part 2

--- 2025/src/bin/02.rs
+++ 2025/src/bin/02.rs
@@ -1,26 +1,40 @@
 advent_of_code::solution!(2);
 
-fn is_invalid(id: u64) -> bool {
-    let id_str = format!("{id}");
-    if id_str.len() % 2 > 0 {
-        false
-    } else {
-        let (a, b) = id_str.split_at(id_str.len() / 2);
-        a == b
+fn parse(input: &str) -> Vec<(u64, u64)> {
+    input
+        .split(',')
+        .map(|r| {
+            let (start_str, end_str) = r.split_once('-').unwrap();
+            let start: u64 = start_str.parse().expect("not an int");
+            let end: u64 = end_str.parse().expect("not an int");
+            (start, end)
+        })
+        .collect()
+}
+
+fn repeats(s: &str, len: usize) -> bool {
+    if s.len() % len != 0 {
+        return false;
     }
+    let b = s.as_bytes();
+    for i in 0..len {
+        let c = b[i];
+        for r in 1..b.len() / len {
+            if b[r * len + i] != c {
+                return false;
+            }
+        }
+    }
+    true
 }
 
 pub fn part_one(input: &str) -> Option<u64> {
-    let ranges = input.split(',').map(|r| {
-        let (start_str, end_str) = r.split_once('-').unwrap();
-        let start: u64 = start_str.parse().expect("not an int");
-        let end: u64 = end_str.parse().expect("not an int");
-        (start, end)
-    });
+    let ranges = parse(input);
     let mut total: u64 = 0;
     for (start, end) in ranges {
         for id in start..=end {
-            if is_invalid(id) {
+            let id_str = format!("{id}");
+            if id_str.len() % 2 == 0 && repeats(&id_str, id_str.len() / 2) {
                 total += id;
             }
         }
@@ -29,7 +43,20 @@ pub fn part_one(input: &str) -> Option<u64> {
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
-    None
+    let ranges = parse(input);
+    let mut total: u64 = 0;
+    for (start, end) in ranges {
+        for id in start..=end {
+            let id_str = format!("{id}");
+            for len in 1..=id_str.len() / 2 {
+                if repeats(&id_str, len) {
+                    total += id;
+                    break;
+                }
+            }
+        }
+    }
+    Some(total)
 }
 
 #[cfg(test)]
@@ -45,6 +72,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(4174379265));
     }
 }
