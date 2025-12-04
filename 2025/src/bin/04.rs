67420d2eec6cd5269d1ee0f6fd95e44a580814c2
Tim Danner <tim@danners.us>
2025-12-03 23:44:16 -0600

    2025 day 04 part 1

---
+++ 2025/src/bin/04.rs
@@ -0,0 +1,126 @@
+advent_of_code::solution!(4);
+
+#[derive(Clone, Copy, PartialEq)]
+enum Contents {
+    Empty,
+    Paper,
+}
+
+#[derive(Clone, Copy, PartialEq)]
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+struct Map {
+    tiles: Vec<Vec<Contents>>,
+}
+
+impl Map {
+    pub fn at(&self, p: &Point) -> Contents {
+        if p.y >= 0 && p.y < self.tiles.len() as i32 {
+            let row = &self.tiles[p.y as usize];
+            if p.x >= 0 && p.x < row.len() as i32 {
+                row[p.x as usize]
+            } else {
+                Contents::Empty
+            }
+        } else {
+            Contents::Empty
+        }
+    }
+
+    pub fn y_range(&self) -> std::ops::Range<usize> {
+        0..self.tiles.len()
+    }
+
+    pub fn x_range(&self) -> std::ops::Range<usize> {
+        0..self.tiles[0].len()
+    }
+}
+
+fn neighbors_of(p: &Point) -> Vec<Point> {
+    vec![
+        Point {
+            x: p.x - 1,
+            y: p.y - 1,
+        },
+        Point { x: p.x, y: p.y - 1 },
+        Point {
+            x: p.x + 1,
+            y: p.y - 1,
+        },
+        Point { x: p.x - 1, y: p.y },
+        Point { x: p.x + 1, y: p.y },
+        Point {
+            x: p.x - 1,
+            y: p.y + 1,
+        },
+        Point { x: p.x, y: p.y + 1 },
+        Point {
+            x: p.x + 1,
+            y: p.y + 1,
+        },
+    ]
+}
+
+fn parse(input: &str) -> Map {
+    Map {
+        tiles: input
+            .lines()
+            .map(|l| {
+                l.chars()
+                    .map(|c| match c {
+                        '.' => Contents::Empty,
+                        '@' => Contents::Paper,
+                        _ => panic!(),
+                    })
+                    .collect()
+            })
+            .collect(),
+    }
+}
+
+pub fn part_one(input: &str) -> Option<u64> {
+    let map = parse(input);
+    let mut accessible = 0u64;
+    for y in map.y_range() {
+        for x in map.x_range() {
+            let p = Point {
+                x: x as i32,
+                y: y as i32,
+            };
+            if map.at(&p) == Contents::Paper {
+                let paper_count = neighbors_of(&p)
+                    .iter()
+                    .filter(|p| map.at(*p) == Contents::Paper)
+                    .count();
+                if paper_count < 4 {
+                    accessible += 1;
+                }
+            }
+        }
+    }
+    Some(accessible)
+}
+
+pub fn part_two(input: &str) -> Option<u64> {
+    None
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(13));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, None);
+    }
+}
