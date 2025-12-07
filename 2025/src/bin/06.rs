ed768e1210557b3a7b26b559d1c611a82e3e1b17
Tim Danner <tim@danners.us>
2025-12-07 08:16:18 -0600

    2025 day 06 part 2

--- 2025/src/bin/06.rs
+++ 2025/src/bin/06.rs
@@ -35,8 +35,51 @@ pub fn part_one(input: &str) -> Option<u64> {
     Some(total)
 }
 
-pub fn part_two(_: &str) -> Option<u64> {
-    None
+pub fn part_two(input: &str) -> Option<u64> {
+    let text: Vec<Vec<char>> = input.lines().map(|l| l.chars().collect()).collect();
+
+    // logic assumes all lines are exactly the same length
+    let cols = text[0].len();
+    assert!(text.iter().all(|l| l.len() == cols));
+
+    let mut operands = Vec::new();
+    let mut total = 0u64;
+    for col in (0..cols).rev() {
+        let mut operand: Option<u64> = None;
+        let mut operator: Option<char> = None;
+        for line in text.iter() {
+            let c = line[col];
+            match c {
+                ' ' => continue,
+                '0'..='9' => {
+                    let digit = (c as u8 - '0' as u8) as u64;
+                    operand = match operand {
+                        None => Some(digit),
+                        Some(value) => Some(value * 10 + digit),
+                    };
+                }
+                '+' | '*' => operator = Some(c),
+                _ => panic!("unexpected char {c}"),
+            }
+        }
+        if let Some(operand) = operand {
+            operands.push(operand);
+        }
+        match operator {
+            None => {}
+            Some(op) => {
+                let value: u64 = match op {
+                    '+' => operands.iter().sum(),
+                    '*' => operands.iter().product(),
+                    _ => panic!("unknown operator {op}"),
+                };
+                total += value;
+                operands.clear();
+            }
+        }
+    }
+
+    Some(total)
 }
 
 #[cfg(test)]
@@ -52,6 +95,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(3263827));
     }
 }
