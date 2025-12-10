34dd72b2d5c16da1ebb8f579ed55c0c3ff5e9291
Tim Danner <tim@danners.us>
2025-12-09 22:57:38 -0600

    2025 day 09 part 2

--- 2025/src/bin/09.rs
+++ 2025/src/bin/09.rs
@@ -1,4 +1,4 @@
-use std::{cmp::max, u64};
+use std::{cmp::max, cmp::min, u64};
 
 advent_of_code::solution!(9);
 
@@ -32,7 +32,7 @@ impl Edge {
             if p1.x < p2.x {
                 Self { p1: *p1, p2: *p2 }
             } else {
-                Self { p2: *p2, p1: *p1 }
+                Self { p1: *p2, p2: *p1 }
             }
         }
     }
@@ -42,6 +42,17 @@ impl Edge {
             Orientation::Vertical
         } else {
             Orientation::Horizontal
+        }
+    }
+
+    fn crosses_rect(&self, xmin: u64, ymin: u64, xmax: u64, ymax: u64) -> bool {
+        match self.orientation() {
+            Orientation::Vertical => {
+                self.p1.x > xmin && self.p1.x < xmax && self.p1.y < ymax && self.p2.y > ymin
+            }
+            Orientation::Horizontal => {
+                self.p1.y > ymin && self.p1.y < ymax && self.p1.x < xmax && self.p2.x > xmin
+            }
         }
     }
 
@@ -94,17 +105,15 @@ fn area(p1: &Point, p2: &Point) -> u64 {
 }
 
 fn point_inside(p: &Point, verts: &Vec<Edge>) -> bool {
-    // we are going to cast a ray from p to (+Inf, p.y) and count intersections
-    let p2 = Point {
-        x: u64::MAX,
-        y: p.y,
-    };
+    // we are going to cast a ray from p to (0, p.y) and count intersections
+    let p2 = Point { x: 0, y: p.y };
     let ray = Edge::new(p, &p2);
     let intersections: Vec<Edge> = verts
         .iter()
         .filter(|e| e.intersects(&ray))
         .copied()
         .collect();
+    // println!("ray {ray:?} has intersections {intersections:?}");
     let inside = !intersections.len().is_multiple_of(2);
     // let label = if inside { "inside" } else { "outside" };
     // println!("{p:?} is {label}");
@@ -128,36 +137,36 @@ pub fn part_two(input: &str) -> Option<u64> {
     let points = parse(input);
 
     // make a list of horizontal and vertical edges
-    let edges = points
+    let edges: Vec<Edge> = points
         .iter()
         .enumerate()
-        .map(|(i, p1)| Edge::new(p1, &points[(i + 1) % points.len()]));
-    let (verticals, horizontals): (Vec<Edge>, Vec<Edge>) =
-        edges.partition(|e| e.orientation() == Orientation::Vertical);
-
-    // test that the polygon is regular (not self-intersecting)
-    for hor in horizontals.iter() {
-        for vert in verticals.iter() {
-            if hor.intersects(vert) {
-                panic!("polygon intersects self: {hor:?} crosses {vert:?}");
-            }
-        }
-    }
+        .map(|(i, p1)| Edge::new(p1, &points[(i + 1) % points.len()]))
+        .collect();
+    let (verticals, _horizontals): (Vec<Edge>, Vec<Edge>) = edges
+        .iter()
+        .partition(|e| e.orientation() == Orientation::Vertical);
 
     // To find the largest rectangle that is inside the polygon, we are going to
     // consider all rectangles formed by pairs of vertices, checking whether the
-    // opposite corners are also inside the polygon. Basically the same
-    // algorithm from part 1, but with the "point_inside" check.
+    // center is inside the polygon and no edge of the polygon crosses the
+    // interior of the rectangle.
     let mut max_area: u64 = 0;
+    // let p1 = &Point { x: 9, y: 5 };
+    // let p2 = &Point { x: 2, y: 3 };
     for p1 in points.iter() {
         for p2 in points.iter() {
-            let p3 = Point { x: p1.x, y: p2.y };
-            let p4 = Point { x: p2.x, y: p1.y };
-            if point_inside(&p3, &verticals) && point_inside(&p4, &verticals) {
-                // max_area = max(max_area, area(p1, p2));
-                let a = area(p1, p2);
-                if a > max_area {
-                    // println!("rect {p1:?} / {p2:?} has area {a}");
+            let a = area(p1, p2);
+            if a > max_area {
+                // no point in testing a rectangle that isn't bigger
+                let (xmin, xmax) = (min(p1.x, p2.x), max(p1.x, p2.x));
+                let (ymin, ymax) = (min(p1.y, p2.y), max(p1.y, p2.y));
+                let center = Point {
+                    x: (p1.x + p2.x) / 2,
+                    y: (p1.y + p2.y) / 2,
+                };
+                if point_inside(&center, &verticals)
+                    && !edges.iter().any(|e| e.crosses_rect(xmin, ymin, xmax, ymax))
+                {
                     max_area = a;
                 }
             }
@@ -181,22 +190,5 @@ mod tests {
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
         assert_eq!(result, Some(24));
-    }
-
-    #[test]
-    fn test_point_inside() {
-        let input = &advent_of_code::template::read_file("examples", DAY);
-        let points = parse(input);
-
-        // make a list of horizontal and vertical edges
-        let edges = points
-            .iter()
-            .enumerate()
-            .map(|(i, p1)| Edge::new(p1, &points[(i + 1) % points.len()]));
-        let (verticals, _horizontals): (Vec<Edge>, Vec<Edge>) =
-            edges.partition(|e| e.orientation() == Orientation::Vertical);
-
-        let result = point_inside(&Point { x: 2, y: 5 }, &verticals);
-        assert_eq!(result, true);
     }
 }
